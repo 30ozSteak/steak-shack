e31916c189769a11edd25961c45528b5abf3216f
Nick Dambrosio <Nick.DAmbrosio@regiscompany.com>
2020-06-16 10:54:58 -0600

    wrap entire app in wrapRootElement in Gatsby Browser

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Nicks Steak Shack`,
+    description: `A place to buy steak.`,
+    author: `@iaaafm`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
