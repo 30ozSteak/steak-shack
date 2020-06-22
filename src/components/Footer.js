5ffbc3de415497c3c37fd0e625c59915700530c2
Nick Dambrosio <ndambro303@gmail.com>
2020-06-22 10:42:49 -0600

    clean up package.json

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,26 @@
 import React from "react"
+import { AiOutlineTwitter } from "react-icons/ai"
 
 const Footer = () => (
-  <footer
-    className="footer"
-    style={{ background: "var(--darkPurp)", color: "white" }}
-  >
-    © {new Date().getFullYear()}, Built with
-    {` `}
-    <a href="https://www.gatsbyjs.org">Gatsby</a>
+  <footer className="footer" style={{ background: "#F7F7F7", color: "black" }}>
+    <p>Customer Service</p>{" "}
+    <ul>
+      <li>Help / FAQ</li>
+      <li>Returns and Exchanges</li>
+      <li>Contact Us</li>
+    </ul>
+    <ul>
+      <li>Shop</li>
+      <li>Blog</li>
+      <li>About our stickers</li>
+    </ul>
+    <div>
+      <AiOutlineTwitter />
+      <p>
+        © {new Date().getFullYear()}, Built with
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </p>
+    </div>
   </footer>
 )
 
