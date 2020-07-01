94b3685891ae44de977199914c549516b07277be
Nick Dambrosio <ndambro303@gmail.com>
2020-07-01 13:03:51 -0600

    good morning

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -1,60 +1,60 @@
-import React from "react"
-import Image from "gatsby-image"
-import { useStaticQuery, graphql, Link } from "gatsby"
+// import React from "react"
+// import Image from "gatsby-image"
+// import { useStaticQuery, graphql, Link } from "gatsby"
 
-const Featured = () => {
-  const { shopifyProduct: product } = useStaticQuery(
-    graphql`
-      query featured {
-        shopifyProduct(vendor: { eq: "featured" }) {
-          id
-          handle
-          title
-          description
-          variants {
-            id
-            shopifyId
-            title
-            price
-            sku
-            availableForSale
-          }
-          images {
-            id
-            localFile {
-              childImageSharp {
-                fluid(maxWidth: 1920, maxHeight: 1080) {
-                  ...GatsbyImageSharpFluid_withWebp
-                  ...GatsbyImageSharpFluidLimitPresentationSize
-                }
-              }
-            }
-          }
-        }
-      }
-    `
-  )
+// const Featured = () => {
+//   const { shopifyProduct: product } = useStaticQuery(
+//     graphql`
+//       query featured {
+//         shopifyProduct(vendor: { eq: "featured" }) {
+//           id
+//           handle
+//           title
+//           description
+//           variants {
+//             id
+//             shopifyId
+//             title
+//             price
+//             sku
+//             availableForSale
+//           }
+//           images {
+//             id
+//             localFile {
+//               childImageSharp {
+//                 fluid(maxWidth: 1920, maxHeight: 1080) {
+//                   ...GatsbyImageSharpFluid_withWebp
+//                   ...GatsbyImageSharpFluidLimitPresentationSize
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `
+//   )
 
-  const {
-    images: [firstImage],
-    variants: [firstVariant],
-  } = product
+//   const {
+//     images: [firstImage],
+//     variants: [firstVariant],
+//   } = product
 
-  return (
-    <div className="featured-container">
-      <div className="first-featured">
-        <Image fluid={firstImage.localFile.childImageSharp.fluid} />
-        <button className="button is-dark">
-          <Link
-            style={{ color: "white", padding: "2rem" }}
-            to={`/product/${product.handle}`}
-          >
-            Shop Stickers
-          </Link>
-        </button>
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div className="featured-container">
+//       <div className="first-featured">
+//         <Image fluid={firstImage.localFile.childImageSharp.fluid} />
+//         <button className="button is-dark">
+//           <Link
+//             style={{ color: "white", padding: "2rem" }}
+//             to={`/product/${product.handle}`}
+//           >
+//             Shop Stickers
+//           </Link>
+//         </button>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default Featured
+// export default Featured
--- src/components/Video/VideoPlayer.js
+++ src/components/Video/VideoPlayer.js
@@ -8,8 +8,10 @@ const VideoPlayer = () => {
     <div className="video-container">
       <div className="collection-container">
         <h1>New at iaaafm.dev</h1>
-        <p>Shop the Premium Cut Collection</p>
+        <p>The Premium-Cut Collection</p>
       </div>
+      <div className="video-footer">Shop Now</div>
+
       <video className="video-tag" autoPlay loop muted>
         <source src={steakVideo} type="video/mp4" autoPlay />
       </video>
