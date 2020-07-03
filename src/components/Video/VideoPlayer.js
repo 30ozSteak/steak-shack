01c4520cd8aec6ee391af65eb8fb306b38cbb8c8
Nick Dambrosio <ndambro303@gmail.com>
2020-07-02 18:34:43 -0600

    remove video from header

--- src/components/Video/VideoPlayer.js
+++ src/components/Video/VideoPlayer.js
@@ -6,12 +6,7 @@ import "./VideoPlayer.scss"
 const VideoPlayer = () => {
   return (
     <div className="video-container">
-      <div className="collection-container">
-        <h1>Now Available</h1>
-        <p>The Premium-Cut Collection</p>
-      </div>
       <div className="video-footer">Shop Now</div>
-
       <video className="video-tag" autoPlay loop muted preload>
         <source src={steakVideo} type="video/mp4" autoPlay loop muted preload />
       </video>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,18 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-// import Featured from "../components/Featured/Featured"
 import VideoPlayer from "../components/Video/VideoPlayer"
-import "../Fonts/TTCommons-Medium.woff"
 import ProductsListing from "../components/ProductsListing/ProductsListing"
 
 import { Breadcrumb } from "gatsby-plugin-breadcrumb"
 
 const IndexPage = () => (
   <div>
-    {/* <Banner /> */}
     <Layout>
       <SEO title="Home" />
-
-      <VideoPlayer />
       {/* <Featured /> */}
       <ProductsListing />
+      <VideoPlayer />
     </Layout>
   </div>
 )
