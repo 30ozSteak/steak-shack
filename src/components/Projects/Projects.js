b5e8d2a94dd091086bebda5c66a6a6f351d237db
Nick Dambrosio <ndambro303@gmail.com>
2020-07-26 21:20:28 -0600

    gnight

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,7 +6,16 @@ const Projects = () => {
     <div className="projects">
       <h2 id="work">Recent Work.</h2>
       <article className="project-card">
-        <h3 className="project-title">ai enhanced crypto tracker / analyst</h3>
+        <h3 className="project-title">
+          Building a sophisticated <br /> cryptocurrency analyst
+        </h3>
+        <button class="button is-dark">View Project</button>
+      </article>
+      <article className="project-card">
+        <h3 className="project-title">
+          Maintaining mental health <br /> and navigating uncertainty during
+          COVID-19
+        </h3>
         <button class="button is-dark">View Project</button>
       </article>
     </div>
